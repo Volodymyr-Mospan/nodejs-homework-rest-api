13251e7b0a60a8e4c9f0e11a7637bd98e972aae1
Volodymyr <mospan.volodymyr@gmail.com>
2023-05-24 22:37:34 +0300

    completes registration logic

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ const register = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user) {
-    throw HttpError(409, "Email already in use");
+    throw HttpError(409, "Email in use");
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
@@ -15,7 +15,7 @@ const register = async (req, res) => {
   const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
     email: newUser.email,
-    password: hashPassword,
+    subscription: newUser.subscription,
   });
 };
 
